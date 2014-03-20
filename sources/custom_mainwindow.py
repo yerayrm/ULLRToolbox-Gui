50198048712d97237b6188216f869a8ea7491c96
Yeray Rguez <yerayrm90@gmail.com>
2014-03-20 21:29:21 +0000

    Guardado y manejo de variables

--- sources/custom_mainwindow.py
+++ sources/custom_mainwindow.py
@@ -8,14 +8,18 @@ class CustomMainWindow(mw.Ui_MainWindow):
 		super(CustomMainWindow, self).setupUi(MainWindow)
 
 		# actions
-		self.act_abrir = QtGui.QAction(QtGui.QIcon('./resources/openIcon.png'), "open", MainWindow)
+		self.act_abrir = QtGui.QAction(QtGui.QIcon('./resources/openIcon.png'), 'open', MainWindow)
 		self.act_guardar = QtGui.QAction(QtGui.QIcon('./resources/saveIcon.png'), "save", MainWindow)
 
 		# toolBar
 		self.toolBar.addAction(self.act_abrir)
 		self.toolBar.addAction(self.act_guardar)
 
+		self.mnu_archivo.addAction(self.act_abrir)
+		#self.menubar.addAction(self.mnu_archivo.menuAction())
+
 		# signals
+		#QtCore.QObject.connect(self.act_abrir, QtCore.SIGNAL("triggered()"), self.abrirArchivo())
 		QtCore.QObject.connect(self.button_ejecutar, QtCore.SIGNAL("clicked()"), self.insertCommand)
 
 		# call a method to translate interface
@@ -31,18 +35,27 @@ class CustomMainWindow(mw.Ui_MainWindow):
 		self.list_variables.insertItem(5, newItem)
 
 
+	def abrirArchivo(self):
+		#fileName = QtGui.QFileDialog.getOpenFileName(self, tr("Open Image"), "/home/jana", tr("Image Files (*.png *.jpg *.bmp)"))
+		path, filter = QtGui.QFileDialog.getOpenFileName(self, "Find Files")
+
+
 	def insertCommand(self):
 		comando = self.edit_comandos.toPlainText()
 		self.text_result.append("> " + comando)
-		self.text_result.append("")
+		#self.text_result.append("")
 
 		def f(x):
-			#self.text_result.append(x)
 			self.text_result.textCursor().insertText(x)
 		
 		rinterface.set_writeconsole(f)
-		resultado = robjects.r('print('+comando+')')
-		self.text_result.append(resultado)
+		backupList = robjects.globalenv.keys()
+		resultado = robjects.r(comando)
+		currentList = robjects.globalenv.keys()
+		
+		if len(backupList) == len(currentList):
+			self.text_result.append(str(resultado))
+
 		rinterface.set_writeconsole(rinterface.consolePrint)
 
 	
