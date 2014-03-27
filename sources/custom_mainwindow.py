cab97e803a5f4e35104a8d294deb7d4691852fc6
Yeray Rguez <yerayrm90@gmail.com>
2014-03-27 23:26:25 +0000

    Permite ver archivos 'txt' en QTableWidget

--- sources/custom_mainwindow.py
+++ sources/custom_mainwindow.py
@@ -28,6 +28,8 @@ class CustomMainWindow(mw.Ui_MainWindow):
 
 		# init methods
 		self.initToolbox()
+		self.initTable()
+		
 
 		# call a method to translate interface
 		self.retranslateUi(MainWindow)
@@ -52,30 +54,28 @@ class CustomMainWindow(mw.Ui_MainWindow):
 		rinterface.set_writeconsole(rinterface.consolePrint)
 
 
+
 	def insertVariables(self, MainWindow):
 		newItem = QtGui.QListWidgetItem()
 		self.list_variables.insertItem(5, newItem)
 
 
+
 	def abrirArchivo(self):
 		fileName, _ = QtGui.QFileDialog.getOpenFileName(self.mnu_archivo, "Abrir archivo", "./samples", "Archivos de datos (*.txt *.xls *.sav *.Rdata)")
-		self.text_result.append("[FICHERO]" + fileName)
 
 		comando = "datos = lee.archivo.fnc('" + fileName + "')"
-		self.text_result.append("> " + comando)
 
-		def f(x):
-			self.text_result.textCursor().insertText(x)
+		#def f(x):
+		#	self.text_result.textCursor().insertText(x)
 		
-		rinterface.set_writeconsole(f)
+		#rinterface.set_writeconsole(f)
 		backupList = robjects.globalenv.keys()
 		resultado = robjects.r(comando)
 		currentList = robjects.globalenv.keys()
-		
-		if len(backupList) == len(currentList):
-			self.text_result.append(str(resultado))
 
-		rinterface.set_writeconsole(rinterface.consolePrint)
+		#rinterface.set_writeconsole(rinterface.consolePrint)
+		self.completeTable()
 
 
 
@@ -97,3 +97,47 @@ class CustomMainWindow(mw.Ui_MainWindow):
 		rinterface.set_writeconsole(rinterface.consolePrint)
 
 	
+
+	def completeTable(self):
+		nrow = robjects.r("nrow(datos)")
+		ncol = robjects.r("ncol(datos)")
+		print("row " + str(nrow[0]) + " col " + str(ncol))
+
+		self.tableWidget.clear()
+		
+		self.tableWidget.setRowCount(nrow[0])
+		self.tableWidget.setColumnCount(ncol[0])
+
+		for i in range(ncol[0]):
+			ind = i+1
+			nameCol = robjects.r("colnames(datos["+str(ind)+"])")
+			print str(ind) + "-" + str(nameCol[0])
+			newItem = QtGui.QTableWidgetItem(str(nameCol[0]))
+			self.tableWidget.setHorizontalHeaderItem(i, newItem)
+
+		for i in range(nrow[0]):
+			for j in range(ncol[0]):
+				ni = i+1
+				nj = j+1
+				comando = "datos[" + str(ni) + "," + str(nj) + "]"
+				#print comando
+				content = robjects.r(comando)
+				#print comando + " - " + str(content.levels[0])
+				itemContent = QtGui.QTableWidgetItem(str(content.levels[0]))
+				#itemContent = QtGui.QTableWidgetItem(str(content[0]))
+				self.tableWidget.setItem(i, j, itemContent)
+
+
+
+	def initTable(self):
+		nrow = 50
+		ncol = 20
+		
+		self.tableWidget.setRowCount(nrow)
+		self.tableWidget.setColumnCount(ncol)
+
+		for i in range(ncol):
+			ind = i+1
+			header = "Var " + str(ind)
+			newItem = QtGui.QTableWidgetItem(header)
+			self.tableWidget.setHorizontalHeaderItem(i, newItem)
