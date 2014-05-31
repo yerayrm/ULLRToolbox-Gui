1d556c6f7729db85cf6d598a52eb973da45a38ca
Yeray Rguez <yerayrm90@gmail.com>
2014-05-31 12:47:40 +0100

    Discretizar programado

--- resource_rc.py
+++ resource_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Fri May 30 22:57:01 2014
+# Created: Sat May 31 12:44:28 2014
 #      by: The Resource Compiler for PySide (Qt v4.8.0)
 #
 # WARNING! All changes made in this file will be lost!
